490be29bbbc46857c3bed661c54f201deab96a0d
NataliyaShevchuk <nata.shevchuk@icloud.com>
2023-01-11 17:36:31 +0100

    ottavo step completato

--- query.sql
+++ query.sql
@@ -19,9 +19,9 @@ FROM `students`
 WHERE YEAR(`date_of_birth`)<="1993";
 
 -- oppure
-SELECT * 
-FROM `students`
-WHERE (YEAR(CURRENT_DATE)) - (YEAR(`date_of_birth`)) > 30;
+    SELECT * 
+    FROM `students`
+    WHERE (YEAR(CURRENT_DATE)) - (YEAR(`date_of_birth`)) > 30;
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
@@ -44,6 +44,18 @@ WHERE `name` LIKE "Corso di Laurea Magistrale%";
 
 -- 7. Da quanti dipartimenti è composta l'università? (12)
 
-SELECT COUNT(`id`) FROM `departments`;
+SELECT COUNT(`id`) 
+FROM `departments`;
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+
+SELECT COUNT(*) 
+FROM `teachers`
+WHERE `phone` IS NULL;
+
+
+--oppure
+    SELECT * 
+    FROM `teachers`
+    WHERE `phone` IS NULL;
+
