75bdeb2f32f8cc7c2ccb15ab2d597abfd59329a3
NataliyaShevchuk <nata.shevchuk@icloud.com>
2023-01-11 17:02:30 +0100

    settimo step completato

--- query.sql
+++ query.sql
@@ -44,5 +44,6 @@ WHERE `name` LIKE "Corso di Laurea Magistrale%";
 
 -- 7. Da quanti dipartimenti è composta l'università? (12)
 
+SELECT COUNT(`id`) FROM `departments`;
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
