f7896f12a887fa451753d5185ec395babfcdc9a9
NataliyaShevchuk <nata.shevchuk@icloud.com>
2023-01-12 16:01:35 +0100

    step 3 di group by, done

--- group.sql
+++ group.sql
@@ -15,6 +15,9 @@ GROUP BY `edificio`
 
 --3. Calcolare la media dei voti di ogni appello d'esame
 
-
+SELECT AVG(`vote`) AS `voto_medio`,
+	`exam_id` AS `appello`
+FROM `exam_student`
+GROUP BY `appello`
 
 --4. Contare quanti corsi di laurea ci sono per ogni dipartimento
