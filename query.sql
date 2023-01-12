7f46791ed1720dc86a2ac14e00f041562fa8128f
NataliyaShevchuk <nata.shevchuk@icloud.com>
2023-01-12 10:16:18 +0100

    aggiunto opzione c allo step tre

--- query.sql
+++ query.sql
@@ -23,6 +23,11 @@ WHERE YEAR(`date_of_birth`)<="1993";
     FROM `students`
     WHERE (YEAR(CURRENT_DATE)) - (YEAR(`date_of_birth`)) > 30;
 
+    --oppure con Timestampdiff
+    SELECT *, TIMESTAMPDIFF(YEAR, `date_of_birth`, NOW())
+    FROM `students`
+    WHERE TIMESTAMPDIFF(YEAR, `date_of_birth`, NOW()) > 30;
+
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
 SELECT * 
